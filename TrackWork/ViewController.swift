cd15dd55c35f9874bc8b1a8e1f333ccd74d82eb2
Chrystian Salgado <salllgado@hotmail.com.br>
2019-03-19 11:44:51 -0300

    Initial Commit

---
+++ TrackWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackWork
+//
+//  Created by Chrystian Salgado on 19/03/19.
+//  Copyright © 2019 Chrystian Salgado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TrackWorkTests/TrackWorkTests.swift
@@ -0,0 +1,34 @@
+//
+//  TrackWorkTests.swift
+//  TrackWorkTests
+//
+//  Created by Chrystian Salgado on 19/03/19.
+//  Copyright © 2019 Chrystian Salgado. All rights reserved.
+//
+
+import XCTest
+@testable import TrackWork
+
+class TrackWorkTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
