dbdb8af2f066fa3e98fc96ba51ffd2de9646a57d
Chrystian Salgado <salllgado@hotmail.com.br>
2019-03-22 12:03:13 -0300

    Adicionando segmentedControll e Switch da view

--- TrackWork/Source/ViewController.swift
+++ TrackWork/Source/ViewController.swift
@@ -8,15 +8,22 @@
 
 import UIKit
 
+enum AvailableViews {
+    case manualView
+    case automaticView
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var btnCheckIn: UIButton!
     @IBOutlet weak var lbDescription: UILabel!
     @IBOutlet weak var lbTimeToGo: UILabel!
+    @IBOutlet weak var segmentedControll: UISegmentedControl!
+    @IBOutlet weak var manualContainerView: UIView!
+    @IBOutlet weak var automaticContainerView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setUIPreferences()
     }
     
@@ -47,20 +54,43 @@ class ViewController: UIViewController {
 //        btnCheckIn.isEnabled = false
     }
     
+    private func switchViews(hidden availableView: AvailableViews) {
+        availableView == .manualView ? (automaticContainerView.isHidden = true) : (automaticContainerView.isHidden = false)
+        manualContainerView.isHidden =  !automaticContainerView.isHidden
+    }
+    
     @IBAction func actionCheckin(_ sender: Any) {
         saveData()
         animateDisabledButton()
+    }
+    
+    @IBAction func switchSegmentedControll(_ sender: Any) {
+        switch segmentedControll.selectedSegmentIndex {
+        case 0:
+            switchViews(hidden: .automaticView)
+            break
+        case 1:
+            switchViews(hidden: .manualView)
+            break
+        default:
+            break
+        }
     }
 }
 
 extension ViewController {
     
     fileprivate func setUIPreferences() {
-        self.title = "Add Track"
-        self.navigationController?.navigationBar.prefersLargeTitles = true
-        self.navigationController?.navigationBar.tintColor = .white
-        self.navigationController?.navigationBar.backgroundColor = .blue
+        // Nagivation Bar Configuration.
+        title = "Add Track"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.backgroundColor = .blue
         
+        // Outlets Configuration.
+        segmentedControll.setTitle("Modo Automatico", forSegmentAt: 0)
+        segmentedControll.setTitle("Modo Manual", forSegmentAt: 1)
+//        segmentedControll.tintColor = navigationController?.navigationBar.backgroundColor
         btnCheckIn.layer.cornerRadius = (btnCheckIn.frame.height/2)
         btnCheckIn.setTitle("Bater Ponto", for: .normal)
         lbDescription.text = "Aqui você bate seu ponto e a gente te avisa do resto. Vamos lá."
